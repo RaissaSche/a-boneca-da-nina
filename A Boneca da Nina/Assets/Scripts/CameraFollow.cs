e17cf540af4c790df06e7d06fa9b2d8370bd51e8
Raissa Scheeren <raissasche@gmail.com>
2021-11-22 22:13:47 -0300

    fixing camera and bridge bugs

---
+++ A Boneca da Nina/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    [Range(2, 10)]
+    public float smoothFactor;
+
+    private void LateUpdate()
+    {
+        Follow();
+    }
+
+    void Follow()
+    {
+        Vector3 offsettedTargetPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, offsettedTargetPosition, smoothFactor * Time.fixedDeltaTime);
+        transform.position = offsettedTargetPosition;
+    }
+}
